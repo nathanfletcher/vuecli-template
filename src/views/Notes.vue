e607b88df2e3d354ba78458e0bb5fa432b294346
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2018-04-12 07:42:52 +0000

    Cleaned up notes and added method shell to delete note

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -24,27 +24,6 @@
     </li>
   </ul>
   </div>
-
-
-  <!--<div >
-    <div class="mdl-card__title" v-for="post of posts" :key="3">
-      <h2 class="mdl-card__title-text">{{post.title}}</h2>
-    </div>
-    <div class="mdl-card__supporting-text">
-      {{post.body}}
-    </div>
-    <div class="mdl-card__actions mdl-card--border">
-      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-        Delete
-      </a>
-    </div>
-    <div class="mdl-card__menu">
-      <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-        <i class="material-icons">share</i>
-      </button>
-    </div>
-  </div>-->
-
 </template>
 
 <script>
@@ -63,10 +42,14 @@ export default {
     .then(response => {
       // JSON responses are automatically parsed.
       this.posts = response.data
+      console.log(JSON.stringify(this.posts))
     })
     .catch(e => {
       this.errors.push(e)
     })
+  },
+  methods(){
+    
   }
 }
 </script>
